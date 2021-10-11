174769a511227acc3e31246bf186571f36ff815d
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-10-11 15:40:55 +0300

    feat:test one.

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -3,3 +3,11 @@ from math_series.math_series import fibonacci
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_one():
+    # assign => what is the output that i resive
+    actual = fibonacci(6)
+    # arrange => what output do i expect to get
+    expected = 8
+    # assert => check if the output is as expected
+    assert actual == expected
